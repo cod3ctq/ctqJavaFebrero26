d7d67544e7c6ae4d564ddd9e0158c6b3d6291b3c
JOPRAM <jose.ramos.juarz@gmail.com>
2026-02-27 12:17:27 -0600

    segunda practicca javagit

---
+++ javarw_jpr/src/Main.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        String ruta= "C:\\Users\\jopra\\OneDrive\\Desktop\\Practicaclase.txt"; //almacena donde esta guardado el archivo
+        String rutaEscritura="C:\\Users\\jopra\\OneDrive\\Desktop\\Practicaclaseescritura.txt";
+        String linea= "";//guardar temporalmente el contenido de la line aactual que es etste leyendo
+
+        File file = new File(ruta);//Representa al archivo, cargandolo en memoria
+        File escritura = new File(rutaEscritura);
+
+
+        String curp;
+        try{//intenta
+            //hacer copia para leer el contenido
+            FileReader fr = new FileReader(file);//crear un cache del contenido del archivo
+            BufferedReader br = new BufferedReader(fr);//leer el contenido
+            FileWriter fw = new FileWriter(escritura);// Objeto para escribir dentro del archivo
+String [] datos;
+            while ((linea = br.readLine()) !=null){
+                linea = linea.replace(",","");
+                System.out.println(linea);
+//            System.out.println("AP_P:" + linea.substring(7));//substring sirve para cortar cadenas de texto.
+//            System.out.println("AP_P2:" + linea.substring(7,9));
+            datos = linea.split(" ");//
+                //armando el curp
+                curp = datos[1].substring(0,2) +
+                        datos[2].substring(0,1) +
+                        datos[0].substring(0,1)+
+                        datos[3].substring(8)+
+                        datos[3].substring(3,5)+
+                        datos[3].substring(0,2)+
+                        datos[4] + datos[5].substring(0,2) + datos[6];
+                System.out.println(curp);
+
+                //Escribir el curp en el nuevo archivo
+                fw.write(curp+"\n");
+            }
+
+            //cerrar el flujo hacia el archivo escritura
+            fw.close();
+
+        }catch (Exception ex){//atrapando manejando la excepcion
+            ex.printStackTrace();
+        }
+    }
+}
